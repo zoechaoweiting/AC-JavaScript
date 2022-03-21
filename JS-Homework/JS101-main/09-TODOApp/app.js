35b6cab1786e7b2ee5671848e6e850edb97cd871
zoe <chaonbie@gmail.com>
2022-03-21 18:01:41 +0800

    update 其他解法

--- JS-Homework/JS101-main/09-TODOApp/app.js
+++ JS-Homework/JS101-main/09-TODOApp/app.js
@@ -45,3 +45,99 @@ document.querySelectorAll(".closeBtn").forEach((button) => {
     button.parentElement.remove()
   })
 })
+
+// // 老師的做法
+// function createTodoItem(text) {
+//   const todo = document.createElement("li")
+//   todo.className = "todo-item"
+
+//   const span = document.createElement("span")
+//   span.className = "item"
+//   span.textContent = text
+
+//   const btn = document.createElement("button")
+//   btn.className = "closeBtn"
+//   btn.textContent = "X"
+//   btn.addEventListener("click", function (e) {
+//     this.parentElement.remove()
+//   })
+
+//   todo.appendChild(span)
+//   todo.appendChild(btn)
+
+//   return todo
+// }
+
+// function addItemInContainer(container, input) {
+//   if (input.value.trim() !== "") {
+//     const todo = createTodoItem(input.value)
+//     container.insertAdjacentElement("afterbegin", todo)
+//   }
+
+//   input.value = ""
+//   input.focus()
+// }
+
+// function addTodo() {
+//   const container = document.querySelector(".todo-list")
+//   const input = document.querySelector("#taskInput")
+//   addItemInContainer(container, input)
+// }
+
+// document.querySelector("#addBtn").addEventListener("click", addTodo)
+
+// document.querySelector("#taskInput").addEventListener("keyup", (e) => {
+//   if (e.key === "Enter") {
+//     addTodo()
+//   }
+// })
+
+// // 老師的做法2 使用事件委任
+// function createTodoItem(text) {
+//   const todo = document.createElement("li")
+//   todo.className = "todo-item"
+
+//   const span = document.createElement("span")
+//   span.className = "item"
+//   span.textContent = text
+
+//   const btn = document.createElement("button")
+//   btn.className = "closeBtn"
+//   btn.textContent = "X"
+
+//   todo.appendChild(span)
+//   todo.appendChild(btn)
+
+//   return todo
+// }
+
+// function addItemInContainer(container, input) {
+//   if (input.value.trim() !== "") {
+//     const todo = createTodoItem(input.value)
+//     container.insertAdjacentElement("afterbegin", todo)
+//   }
+
+//   input.value = ""
+//   input.focus()
+// }
+
+// function addTodo() {
+//   const container = document.querySelector(".todo-list")
+//   const input = document.querySelector("#taskInput")
+//   addItemInContainer(container, input)
+// }
+
+// document.querySelector("#addBtn").addEventListener("click", addTodo)
+
+// document.querySelector("#taskInput").addEventListener("keyup", (e) => {
+//   if (e.key === "Enter") {
+//     addTodo()
+//   }
+// })
+
+// // 事件委任 event delegation
+// document.querySelector(".todo-list").addEventListener("click", (e) => {
+//   if (e.target.matches("button")) {
+//     e.target.parentElement.remove()
+//   }
+// })
